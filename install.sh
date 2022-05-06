35ce66c51151fdbe4dcf63ce6d18f479be910b93
gusflopes <gusflopes86@gmail.com>
2022-05-06 14:41:47 -0400

    fix: script for installing dependencies

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo Install chromium dependencies for Ubuntu-20.04
+echo ---------
+sudo apt-get install libatk-bridge2.0-0 libcups2 libxkbcommon-x11-0 libgbm1 libpango-1.0-0 libcairo2
+
+echo Finished.
